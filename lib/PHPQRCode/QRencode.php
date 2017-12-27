ef2b245f821121768127a86e2ce03dd5d128283a
StephenLi <StephenLi@augumentum.com.cn>
2017-12-27 19:11:44 +0800

    support create colored qrcode

--- lib/PHPQRCode/QRencode.php
+++ lib/PHPQRCode/QRencode.php
@@ -113,7 +113,7 @@ class QRencode {
     }
 
     //----------------------------------------------------------------------
-    public function encodePNG($intext, $outfile = false,$saveandprint=false)
+    public function encodePNG($intext, $outfile = false, $saveandprint=false, $qrBgColor = Constants::QR_BG_COLOR, $qrCodeColor = Constants::QR_CODE_COLOR)
     {
         try {
             ob_start();
@@ -126,7 +126,7 @@ class QRencode {
 
             $maxSize = (int)(Constants::QR_PNG_MAXIMUM_SIZE / (count($tab)+2*$this->margin));
 
-            QRimage::png($tab, $outfile, min(max(1, $this->size), $maxSize), $this->margin,$saveandprint);
+            QRimage::png($tab, $outfile, min(max(1, $this->size), $maxSize), $this->margin, $saveandprint, $qrBgColor, $qrCodeColor);
         } catch (Exception $e) {
             echo $e->getMessage();
             die();
